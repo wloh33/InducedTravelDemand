9bde5c5e59f0ac00dade40c511e957ebc5d15f7f
wloh33 <93232635+wloh33@users.noreply.github.com>
2023-07-23 21:42:42 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function sumCounties(countyData, facilityType) {
       if (county.lanemiles1) {
         memo.laneMiles += county.lanemiles1
       }
-    } else if (facilityType === 'class2-3') {
+    } else if (facilityType === 'class2') {
       if (county.vmt2) {
         memo.vmt += county.vmt2
       }
@@ -25,6 +25,77 @@ function sumCounties(countyData, facilityType) {
         memo.laneMiles += county.lanemiles2
       }
     }
+
+    } else if (facilityType === 'class3') {
+      if (county.vmt3) {
+        memo.vmt += county.vmt3
+      }
+
+      if (county.lanemiles3) {
+        memo.laneMiles += county.lanemiles3
+      }
+    }
+
+    } else if (facilityType === 'class4') {
+      if (county.vmt4) {
+        memo.vmt += county.vmt4
+      }
+
+      if (county.lanemiles4) {
+        memo.laneMiles += county.lanemiles4
+      }
+    }
+
+    } else if (facilityType === 'class5') {
+      if (county.vmt5) {
+        memo.vmt += county.vmt5
+      }
+
+      if (county.lanemiles5) {
+        memo.laneMiles += county.lanemiles5
+      }
+    }
+
+    } else if (facilityType === 'class6') {
+      if (county.vmt6) {
+        memo.vmt += county.vmt6
+      }
+
+      if (county.lanemiles6) {
+        memo.laneMiles += county.lanemiles6
+      }
+    }
+
+    } else if (facilityType === 'class7') {
+      if (county.vmt7) {
+        memo.vmt += county.vmt7
+      }
+
+      if (county.lanemiles7) {
+        memo.laneMiles += county.lanemiles7
+      }
+    }
+
+    } else if (facilityType === 'class8') {
+      if (county.vmt8) {
+        memo.vmt += county.vmt8
+      }
+
+      if (county.lanemiles8) {
+        memo.laneMiles += county.lanemiles8
+      }
+    }
+
+    } else if (facilityType === 'class9') {
+      if (county.vmt9) {
+        memo.vmt += county.vmt9
+      }
+
+      if (county.lanemiles9) {
+        memo.laneMiles += county.lanemiles9
+      }
+    }
+
     return memo
   }, {vmt: 0, laneMiles: 0})
 }
@@ -61,7 +132,35 @@ $('[name="facilityType"]').change(function() {
   if (facilityType === 'class1') {
     $('#selectMSA').parents('.form-group').slideDown()
     $('#selectCounty').parents('.form-group').hide()
-  } else if(facilityType === 'class2-3') {
+  } else if(facilityType === 'class2') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class3') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class4') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class5') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class6') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class7') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class8') {
+    $('#selectCounty').parents('.form-group').slideDown()
+    $('#selectMSA').parents('.form-group').hide()
+  }
+  } else if(facilityType === 'class9') {
     $('#selectCounty').parents('.form-group').slideDown()
     $('#selectMSA').parents('.form-group').hide()
   }
@@ -113,10 +212,30 @@ $('#vmtForm').submit(function(e) {
     countyData = _.filter(counties, function(item) {
       return _.includes(msaData.counties, item.county)
     })
-  } else if (facilityType === 'class2-3') {
+  } else if (facilityType === 'class2') {
     countyData = _.filter(counties, {county: county.toUpperCase()})
   }
-
+  } else if (facilityType === 'class3') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
+  } else if (facilityType === 'class4') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
+  } else if (facilityType === 'class5') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
+  } else if (facilityType === 'class6') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
+  } else if (facilityType === 'class7') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
+  } else if (facilityType === 'class8') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
+  } else if (facilityType === 'class9') {
+    countyData = _.filter(counties, {county: county.toUpperCase()})
+  }
   var data = sumCounties(countyData, facilityType)
   var elasticity = facilityType === 'class1' ? 1 : 0.75
   var newVMT = Math.round(newLaneMiles / data.laneMiles * data.vmt * elasticity * 10) / 10
@@ -124,19 +243,20 @@ $('#vmtForm').submit(function(e) {
   $('#resultsNone').toggle(data.laneMiles === 0);
   $('#resultsExist').toggle(data.laneMiles !== 0);
 
-  if (msa === 'Napa') {
+ /* if (msa === 'Napa') {
     $('#resultsNoneNapa').show()
     $('#resultsNoneOther').hide()
   } else {
     $('#resultsNoneNapa').hide()
     $('#resultsNoneOther').show()
   }
-
+*/
+  
   $('#resultsMain').text(newVMT + ' million additional VMT/year')
 
   if (facilityType === 'class1') {
     $('#geographyName').text(msa + ' MSA')
-    $('#facilityType').text('Interstate highway')
+    $('#facilityType').text('Urban interstate, freeway/expressway')
     $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
     $('#currentVMT').text(data.vmt + ' million')
     $('#elasticity').text('1.0')
@@ -144,9 +264,10 @@ $('#vmtForm').submit(function(e) {
     $('#newVMT').text(newVMT + ' million')
     $('#msaNotes').html('<p><small>' + msa + ' MSA consists of ' + countyData.length + ' ' + pluralize('county','counties', countyData.length) + ' (' + formatCountyList(countyData) + ').</small></p>')
     $('#geographyNameNone').text(msa + ' MSA')
-  } else if (facilityType === 'class2-3') {
+    
+  } else if (facilityType === 'class2') {
     $('#geographyName').text(county + ' County')
-    $('#facilityType').text('Caltrans-managed class 2 and 3 facilities')
+    $('#facilityType').text('Urban principal arterial')
     $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
     $('#currentVMT').text(data.vmt + ' million')
     $('#elasticity').text('0.75')
@@ -155,7 +276,83 @@ $('#vmtForm').submit(function(e) {
     $('#msaNotes').html('')
     $('#geographyNameNone').text(county + ' County')
   }
-
+  } else if (facilityType === 'class3') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Urban minor arterial')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
+ } else if (facilityType === 'class4') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Urban collector')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
+ } else if (facilityType === 'class5') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Urban local road')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
+ } else if (facilityType === 'class6') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Rural interstate and principal arterial')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
+ } else if (facilityType === 'class7') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Rural minor arterial')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
+ } else if (facilityType === 'class8') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Rural major and minor collector')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
+ } else if (facilityType === 'class9') {
+    $('#geographyName').text(county + ' County')
+    $('#facilityType').text('Rural local road')
+    $('#currentLaneMiles').text(data.laneMiles + ' lane miles')
+    $('#currentVMT').text(data.vmt + ' million')
+    $('#elasticity').text('0.75')
+    $('#newLaneMiles').text(newLaneMiles + ' lane miles')
+    $('#newVMT').text(newVMT + ' million')
+    $('#msaNotes').html('')
+    $('#geographyNameNone').text(county + ' County')
+ }
   $('#results').show()
   page.animate({ scrollTop: $(document).height() }, 2000)
 
